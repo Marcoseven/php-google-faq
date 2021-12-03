668d6c7fa46211aad55886d6b22594fb61c23650
Marcoseven <marco.caliciotti3@gmail.com>
2021-12-03 15:25:40 +0100

    Creato file index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/* 
+Traccia esercizio:
+Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+*/
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- #Site_Header -->
+    <header id="Site_Header">
+
+    </header>
+    <!-- /#Site_Header -->
+
+    <!-- #Site_Main -->
+    <main id="Site_Main"></main>
+    <!-- /#Site_Main -->
+
+
+    
+</body>
+</html>
